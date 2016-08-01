a43e7b33883e4d55854d8d0450ccdb07bb430679
Jonathan Harlan <jjharlan06@gmail.com>
2016-08-01 10:44:44 -0400

    Initial Commit

---
+++ LaxRank03/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LaxRank03
+//
+//  Created by JONATHAN HARLAN on 8/1/16.
+//  Copyright © 2016 JONATHAN HARLAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ LaxRank03Tests/LaxRank03Tests.swift
@@ -0,0 +1,36 @@
+//
+//  LaxRank03Tests.swift
+//  LaxRank03Tests
+//
+//  Created by JONATHAN HARLAN on 8/1/16.
+//  Copyright © 2016 JONATHAN HARLAN. All rights reserved.
+//
+
+import XCTest
+@testable import LaxRank03
+
+class LaxRank03Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
